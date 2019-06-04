4c0a0003980684cbd153bb0b38d04e5646631458
unknown-peter <unknown-peter@protonmail.com>
2019-06-04 08:01:48 +0800

    longest_common_prefix solution2

--- src/main/java/com/algorithmproblems/longest_common_prefix/Solution2.java
+++ src/main/java/com/algorithmproblems/longest_common_prefix/Solution2.java
@@ -2,20 +2,25 @@
  * Leetcode - longest_common_prefix
  */
 package com.algorithmproblems.longest_common_prefix;
-import java.util.*;
-import com.ciaoshen.leetcode.util.*;
 
-/** 
- * log instance is defined in Solution interface
- * this is how slf4j will work in this class:
- * =============================================
- *     if (log.isDebugEnabled()) {
- *         log.debug("a + b = {}", sum);
- *     }
- * =============================================
- */
 class Solution2 implements Solution {
 
-    public String longestCommonPrefix(String[] strs) {}
+    /**
+     * solution from leetcode
+     * 依次比较字符串与上次比较得到的最长前缀字符串，如果不是该字符串的最长前缀，则最长前缀字符串长度减一
+     */
+    @Override
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0)
+            return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++)
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty())
+                    return "";
+            }
+        return prefix;
+    }
 
 }
