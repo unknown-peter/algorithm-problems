3efa176f07f08f447303b4c63184ea1c5219e35a
unknown-peter <unknown-peter@protonmail.com>
2019-06-07 08:34:57 +0800

    valid_parentheses、merge_two_sorted_lists

---
+++ src/test/java/com/algorithmproblems/merge_two_sorted_lists/Tester.java
@@ -0,0 +1,144 @@
+/**
+ * Leetcode - merge_two_sorted_lists
+ */
+package com.algorithmproblems.merge_two_sorted_lists;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.ciaoshen.leetcode.util.ListNode;
+
+@RunWith(Parameterized.class)
+public class Tester {
+
+    /**=========================== static for every test cases ============================== */
+
+    // Solution instance to test
+    private static Solution solution;
+    // use this Object to print the log (call from slf4j facade)
+    private static final Logger LOGGER = LoggerFactory.getLogger(TesterRunner.class);
+
+    /** Execute once before any of the test methods in this class. */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        /* uncomment to switch solutions */
+        solution = new Solution1();
+        // solution = new Solution2();
+    }
+
+    /** Execute once after all of the test methods are executed in this class. */
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {}
+
+    /** Initialize test cases */
+    @Parameters
+    public static Collection<Object[]> testcases() {
+        return Arrays.asList(new Object[][] {{"[1,2,4]", "[1,3,4]", "[1,1,2,3,4,4]"},
+                {"[]", "[]", "[]"}, {"[]", "[1,3,5]", "[1,3,5]"}, {"[1,2,4]", "[]", "[1,2,4]"},
+                {"[1,2,3,4]", "[1,2,3,4,5,6,7]", "[1,1,2,2,3,3,4,4,5,6,7]"},
+                {"[1,2,3,4,5,6,7]", "[1,2,3,4]", "[1,1,2,2,3,3,4,4,5,6,7]"},
+                {"[1,3,5,7,9]", "[2,4,6,8,10]", "[1,2,3,4,5,6,7,8,9,10]"}});
+    }
+
+    /**=========================== for each test case ============================== */
+
+    /** 
+     * Parameters for each test (initialized in testcases() method) 
+     * You can change the type of parameters
+     */
+    private String para1; // parameter 1
+    private String para2; // parameter 2
+    private String expected; // parameter 3 (expected answer)
+
+    /** This constructor must be provided to run parameterized test. */
+    public Tester(String para1, String para2, String expected) {
+        // initialize test parameters
+        this.para1 = para1;
+        this.para2 = para2;
+        this.expected = expected;
+    }
+
+    /** Execute before each test method in this class is executed. */
+    @Before
+    public void setUp() throws Exception {}
+
+    /** Executed as a test case. */
+    @Test
+    public void test() {
+
+        Object actual = solution.mergeTwoLists(ConvertClass.stringToListNode(para1),
+                ConvertClass.stringToListNode(para2));
+
+        assertThat(ConvertClass.listNodeToString((ListNode) actual), is(equalTo(expected)));
+
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("mergeTwoLists() pass unit test!");
+        }
+    }
+
+    /** Execute after each test method in this class is executed. */
+    @After
+    public void tearDown() throws Exception {}
+
+}
+
+
+class ConvertClass {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for (int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static ListNode stringToListNode(String input) {
+        // Generate array from the input
+        int[] nodeValues = stringToIntegerArray(input);
+
+        // Now convert that list into linked list
+        ListNode dummyRoot = new ListNode(0);
+        ListNode ptr = dummyRoot;
+        for (int item : nodeValues) {
+            ptr.next = new ListNode(item);
+            ptr = ptr.next;
+        }
+        return dummyRoot.next;
+    }
+
+    public static String listNodeToString(ListNode node) {
+        if (node == null) {
+            return "[]";
+        }
+
+        String result = "";
+        while (node != null) {
+            result += Integer.toString(node.val) + ",";
+            node = node.next;
+        }
+        return "[" + result.substring(0, result.length() - 1) + "]";
+    }
+}
