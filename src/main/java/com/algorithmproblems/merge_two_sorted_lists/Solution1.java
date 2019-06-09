0da804d23ab48304ab54e9281a9b45ee18e22b5b
unknown-peter <unknown-peter@protonmail.com>
2019-06-09 21:44:30 +0800

    merge_two_sorted_lists  optimize

--- src/main/java/com/algorithmproblems/merge_two_sorted_lists/Solution1.java
+++ src/main/java/com/algorithmproblems/merge_two_sorted_lists/Solution1.java
@@ -15,27 +15,23 @@ class Solution1 implements Solution {
         if (l2 == null)
             return l1;
 
-        ListNode me = new ListNode(0);
-        ListNode p = me;
-        while (l1 != null || l2 != null) {
-            if (l1 == null) {
-                p.next = l2;
-                l2 = l2.next;
-            } else if (l2 == null) {
+        ListNode root = new ListNode(0);
+        ListNode p = root;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
                 p.next = l1;
                 l1 = l1.next;
             } else {
-                if (l1.val < l2.val) {
-                    p.next = l1;
-                    l1 = l1.next;
-                } else {
-                    p.next = l2;
-                    l2 = l2.next;
-                }
+                p.next = l2;
+                l2 = l2.next;
             }
             p = p.next;
         }
-        return me.next;
+        if (l1 == null)
+            p.next = l2;
+        if (l2 == null)
+            p.next = l1;
+        return root.next;
     }
 
 }
