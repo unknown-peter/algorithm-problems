ee8fc5ebb57c1c33cf699d1b9833091fbcee6611
unknown-peter <unknown-peter@protonmail.com>
2019-06-11 07:59:10 +0800

    remove_element

---
+++ src/main/java/com/algorithmproblems/remove_element/Solution.java
@@ -0,0 +1,15 @@
+/**
+ * Leetcode - remove_element
+ */
+package com.algorithmproblems.remove_element;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+interface Solution {
+
+    // use this Object to print the log (call from slf4j facade)
+    static Logger log = LoggerFactory.getLogger(Solution.class);
+
+    public int removeElement(int[] nums, int val);
+}
---
+++ src/main/java/com/algorithmproblems/remove_element/Solution1.java
@@ -0,0 +1,29 @@
+/**
+ * Leetcode - remove_element
+ */
+package com.algorithmproblems.remove_element;
+
+class Solution1 implements Solution {
+
+    @Override
+    public int removeElement(int[] nums, int val) {
+        if (nums == null || nums.length == 0)
+            return 0;
+
+        int preIndex = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val)
+                continue;
+
+            // 元素不等于val且两指针不重合的情况下赋值
+            if (i != preIndex) {
+                nums[preIndex] = nums[i];
+                // nums[i] = val;
+            }
+            preIndex++;
+        }
+
+        return preIndex;
+    }
+
+}
