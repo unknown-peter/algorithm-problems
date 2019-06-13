7ff1f1ef712f1b03c595f0202f5277283d75cbae
unknown-peter <unknown-peter@protonmail.com>
2019-06-13 10:36:43 +0800

    search_insert_position、count_and_say

---
+++ src/main/java/com/algorithmproblems/search_insert_position/Solution1.java
@@ -0,0 +1,21 @@
+/**
+ * Leetcode - search_insert_position
+ */
+package com.algorithmproblems.search_insert_position;
+
+class Solution1 implements Solution {
+
+    @Override
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0)
+            return 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= target)
+                return i;
+        }
+
+        return nums.length;
+    }
+
+}
