3d024f02a8bf4c53ce5e47a618d6f6e26206562b
unknown-peter <unknown-peter@protonmail.com>
2019-07-23 23:20:39 +0800

    merge_sorted_array

---
+++ src/main/java/com/algorithmproblems/merge_sorted_array/Solution.java
@@ -0,0 +1,15 @@
+/**
+ * Leetcode - merge_sorted_array
+ */
+package com.algorithmproblems.merge_sorted_array;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+interface Solution {
+
+    // use this Object to print the log (call from slf4j facade)
+    static Logger log = LoggerFactory.getLogger(Solution.class);
+
+    public void merge(int[] nums1, int m, int[] nums2, int n);
+}
