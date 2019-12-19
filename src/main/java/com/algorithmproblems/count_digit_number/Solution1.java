ef3fd61d949a7f431661a336d28326c1be020529
unknown-peter <unknown-peter@protonmail.com>
2019-12-19 23:36:16 +0800

    count_digit_number

---
+++ src/main/java/com/algorithmproblems/count_digit_number/Solution1.java
@@ -0,0 +1,28 @@
+/**
+ * Leetcode - count_digit_number
+ */
+package com.algorithmproblems.count_digit_number;
+
+class Solution1 implements Solution {
+
+    @Override
+    public int count(int num, int digit, boolean isOdd) {
+        if ((num < 0) || (digit < 0) || (digit > 9)) {
+            throw new RuntimeException("argument illegal");
+        }
+        int i = isOdd ? 1 : 0;
+        int count = 0;
+        for (; i <= num; i += 2) {
+            int number = i;
+            while (number > 0) {
+                int numDigit = number % 10;
+                if (numDigit == digit) {
+                    count++;
+                }
+                number /= 10;
+            }
+        }
+        return count;
+    }
+
+}
---
+++ src/main/java/com/algorithmproblems/count_digit_number/count_digit_number.md
@@ -0,0 +1,8 @@
+# Count digit number
+
+计算从0到n的奇数或偶数中有多少个数字m
+
+**示例1：**
+
+    Input n = 866278171, m = 3, isOdd = true
+    Output: 441684627
