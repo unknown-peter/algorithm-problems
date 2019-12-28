ef4aac1c14ed1f336f3d4f25a7ae664638e2b724
unknown-peter <unknown-peter@protonmail.com>
2019-12-28 15:42:51 +0800

    same_tree

---
+++ src/main/java/com/algorithmproblems/same_tree/Solution2.java
@@ -0,0 +1,47 @@
+/**
+ * Leetcode - same_tree
+ */
+package com.algorithmproblems.same_tree;
+
+import java.util.Stack;
+
+import com.ciaoshen.leetcode.util.TreeNode;
+
+class Solution2 implements Solution {
+
+    /**
+     * 前序遍历二叉树并比较节点处值
+     */
+    @Override
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        Stack<TreeNode> pNodeStack = new Stack<>();
+        Stack<TreeNode> qNodeStack = new Stack<>();
+        TreeNode pTmp = p;
+        TreeNode qTmp = q;
+
+        while (true) {
+            if (pTmp != null && qTmp != null) {
+                if (pTmp.val != qTmp.val)
+                    return false;
+                // 中间节点存到栈中，再比较其左节点
+                pNodeStack.push(pTmp);
+                qNodeStack.push(qTmp);
+                pTmp = pTmp.left;
+                qTmp = qTmp.left;
+            } else if (pTmp == null && qTmp == null) {
+                // 全部遍历完成，两个二叉树相等
+                if (pNodeStack.isEmpty() && qNodeStack.isEmpty())
+                    break;
+                if (pNodeStack.isEmpty() || qNodeStack.isEmpty())
+                    return false;
+                // 左节点遍历完之后，从栈中取出中间节点，比较其右节点
+                pTmp = pNodeStack.pop().right;
+                qTmp = qNodeStack.pop().right;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
---
+++ src/test/java/com/algorithmproblems/same_tree/Tester.java
@@ -0,0 +1,144 @@
+/**
+ * Leetcode - same_tree
+ */
+package com.algorithmproblems.same_tree;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.ciaoshen.leetcode.util.TreeNode;
+
+@RunWith(Parameterized.class)
+public class Tester {
+
+    /**=========================== static for every test cases ============================== */
+
+    // Solution instance to test
+    private static Solution solution;
+    // use this Object to print the log (call from slf4j facade)
+    private static final Logger LOGGER = LoggerFactory.getLogger(TesterRunner.class);
+
+    /** Execute once before any of the test methods in this class. */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        /* uncomment to switch solutions */
+        //        solution = new Solution1();
+        solution = new Solution2();
+    }
+
+    /** Execute once after all of the test methods are executed in this class. */
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {}
+
+    /** Initialize test cases */
+    @Parameters
+    public static Collection<Object[]> testcases() {
+        return Arrays.asList(new Object[][] {{"[1,2,3]", "[1,2,3]", true},
+                {"[1,2]", "[1,null,2]", false}, {"[1,2,1]", "[1,1,2]", false}});
+    }
+
+    /**=========================== for each test case ============================== */
+
+    /** 
+     * Parameters for each test (initialized in testcases() method) 
+     * You can change the type of parameters
+     */
+    private String para1; // parameter 1
+    private String para2; // parameter 2
+    private boolean expected; // parameter 3 (expected answer)
+
+    /** This constructor must be provided to run parameterized test. */
+    public Tester(String para1, String para2, boolean expected) {
+        // initialize test parameters
+        this.para1 = para1;
+        this.para2 = para2;
+        this.expected = expected;
+    }
+
+    /** Execute before each test method in this class is executed. */
+    @Before
+    public void setUp() throws Exception {}
+
+    /** Executed as a test case. */
+    @Test
+    public void test() {
+
+        Object actual = solution.isSameTree(ConvertClass.stringToTreeNode(para1),
+                ConvertClass.stringToTreeNode(para2));
+
+        assertThat(actual, is(equalTo(expected)));
+
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("isSameTree() pass unit test!");
+        }
+    }
+
+    /** Execute after each test method in this class is executed. */
+    @After
+    public void tearDown() throws Exception {}
+
+}
+
+
+class ConvertClass {
+    public static TreeNode stringToTreeNode(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return null;
+        }
+
+        String[] parts = input.split(",");
+        String item = parts[0];
+        TreeNode root = new TreeNode(Integer.parseInt(item));
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+
+        int index = 1;
+        while (!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int leftNumber = Integer.parseInt(item);
+                node.left = new TreeNode(leftNumber);
+                nodeQueue.add(node.left);
+            }
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int rightNumber = Integer.parseInt(item);
+                node.right = new TreeNode(rightNumber);
+                nodeQueue.add(node.right);
+            }
+        }
+        return root;
+    }
+}
