b911a989cece6f96741f88c503953eb10213d7c4
unknown-peter <unknown-peter@protonmail.com>
2019-12-28 15:42:51 +0800

    same_tree

---
+++ src/main/java/com/algorithmproblems/same_tree/Solution3.java
@@ -0,0 +1,65 @@
+/**
+ * Leetcode - same_tree
+ */
+package com.algorithmproblems.same_tree;
+
+import java.util.ArrayDeque;
+
+import com.ciaoshen.leetcode.util.TreeNode;
+
+class Solution3 implements Solution {
+
+    public boolean check(TreeNode p, TreeNode q) {
+        // p and q are null
+        if (p == null && q == null)
+            return true;
+        // one of p and q is null
+        if (q == null || p == null)
+            return false;
+        if (p.val != q.val)
+            return false;
+        return true;
+    }
+
+    /**
+     * solution from leetcode
+     */
+    @Override
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        if (!check(p, q))
+            return false;
+
+        // init deques
+        ArrayDeque<TreeNode> deqP = new ArrayDeque<TreeNode>();
+        ArrayDeque<TreeNode> deqQ = new ArrayDeque<TreeNode>();
+        deqP.addLast(p);
+        deqQ.addLast(q);
+
+        while (!deqP.isEmpty()) {
+            p = deqP.removeFirst();
+            q = deqQ.removeFirst();
+
+            if (!check(p, q))
+                return false;
+            if (p != null) {
+                // in Java nulls are not allowed in Deque
+                if (!check(p.left, q.left))
+                    return false;
+                if (p.left != null) {
+                    deqP.addLast(p.left);
+                    deqQ.addLast(q.left);
+                }
+                if (!check(p.right, q.right))
+                    return false;
+                if (p.right != null) {
+                    deqP.addLast(p.right);
+                    deqQ.addLast(q.right);
+                }
+            }
+        }
+        return true;
+    }
+
+}
