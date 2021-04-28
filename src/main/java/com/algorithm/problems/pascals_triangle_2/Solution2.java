62d2337d97496ed37f0c9aa669377a6fea5b2d06
unknown-peter <unknown-peter@protonmail.com>
2021-04-29 07:44:22 +0800

    Pascal's Triangle II solution2

--- src/main/java/com/algorithm/problems/pascals_triangle_2/Solution2.java
+++ src/main/java/com/algorithm/problems/pascals_triangle_2/Solution2.java
@@ -3,12 +3,21 @@
  */
 package com.algorithm.problems.pascals_triangle_2;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 class Solution2 implements Solution {
 
     @Override
     public List<Integer> getRow(int rowIndex) {
-        return null;
+        List<Integer> row = new ArrayList<>(Collections.nCopies(rowIndex + 1, 0));
+        row.set(0, 1);
+        for (int i = 1; i <= rowIndex; i++) {
+            for (int j = i; j > 0; j--) {
+                row.set(j, row.get(j) + row.get(j - 1));
+            }
+        }
+        return row;
     }
 }
