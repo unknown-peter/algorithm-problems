71db25fe7036644b2f74b30262b0cee50ae82604
unknown-peter <unknown-peter@protonmail.com>
2021-04-28 10:30:37 +0800

    Pascal's Triangle II

---
+++ src/main/java/com/algorithm/problems/pascals_triangle_2/Solution1.java
@@ -0,0 +1,27 @@
+/**
+ * Leetcode - pascals_triangle_2
+ */
+package com.algorithm.problems.pascals_triangle_2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution1 implements Solution {
+
+    @Override
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+        row.add(1);
+        if (rowIndex == 0) return row;
+        row.add(1);
+        if (rowIndex == 1) return row;
+        for (int i = 1; i < rowIndex; i++) {
+            for (int j = 0; j < i; j++) {
+                row.add(row.get(0) + row.get(1));
+                row.remove(0);
+            }
+            row.add(1);
+        }
+        return row;
+    }
+}
---
+++ src/main/java/com/algorithm/problems/pascals_triangle_2/Solution2.java
@@ -0,0 +1,14 @@
+/**
+ * Leetcode - pascals_triangle_2
+ */
+package com.algorithm.problems.pascals_triangle_2;
+
+import java.util.List;
+
+class Solution2 implements Solution {
+
+    @Override
+    public List<Integer> getRow(int rowIndex) {
+        return null;
+    }
+}
