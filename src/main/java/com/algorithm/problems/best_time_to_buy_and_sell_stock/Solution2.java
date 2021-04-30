887830417e4348d82f89542e63e56929981e9598
unknown-peter <unknown-peter@protonmail.com>
2021-04-30 08:30:53 +0800

    Best Time to Buy and Sell Stock solution2

--- src/main/java/com/algorithm/problems/best_time_to_buy_and_sell_stock/Solution2.java
+++ src/main/java/com/algorithm/problems/best_time_to_buy_and_sell_stock/Solution2.java
@@ -5,8 +5,19 @@ package com.algorithm.problems.best_time_to_buy_and_sell_stock;
 
 class Solution2 implements Solution {
 
+    /**
+     * solution from leetcode
+     */
     @Override
-    public int maxProfit(int[] prices) {
-        return 0;
+    public int maxProfit(int prices[]) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
     }
 }
