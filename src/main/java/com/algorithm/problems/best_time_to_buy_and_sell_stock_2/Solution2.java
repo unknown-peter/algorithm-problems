39415f0c279e1b0c52c36b39c5850ac1e8dc1907
unknown-peter <unknown-peter@protonmail.com>
2021-05-04 17:53:30 +0800

    Best Time to Buy and Sell Stock II solution2 solution3

--- src/main/java/com/algorithm/problems/best_time_to_buy_and_sell_stock_2/Solution2.java
+++ src/main/java/com/algorithm/problems/best_time_to_buy_and_sell_stock_2/Solution2.java
@@ -5,8 +5,24 @@ package com.algorithm.problems.best_time_to_buy_and_sell_stock_2;
 
 class Solution2 implements Solution {
 
+    /**
+     * solution from leetcode
+     */
     @Override
     public int maxProfit(int[] prices) {
-        return 0;
+        int i = 0;
+        int valley = prices[0];
+        int peak = prices[0];
+        int maxprofit = 0;
+        while (i < prices.length - 1) {
+            while (i < prices.length - 1 && prices[i] >= prices[i + 1])
+                i++;
+            valley = prices[i];
+            while (i < prices.length - 1 && prices[i] <= prices[i + 1])
+                i++;
+            peak = prices[i];
+            maxprofit += peak - valley;
+        }
+        return maxprofit;
     }
 }
---
+++ src/main/java/com/algorithm/problems/best_time_to_buy_and_sell_stock_2/Solution3.java
@@ -0,0 +1,20 @@
+/**
+ * Leetcode - best_time_to_buy_and_sell_stock_2
+ */
+package com.algorithm.problems.best_time_to_buy_and_sell_stock_2;
+
+class Solution3 implements Solution {
+
+    /**
+     * solution from leetcode
+     */
+    @Override
+    public int maxProfit(int[] prices) {
+        int maxprofit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1])
+                maxprofit += prices[i] - prices[i - 1];
+        }
+        return maxprofit;
+    }
+}
