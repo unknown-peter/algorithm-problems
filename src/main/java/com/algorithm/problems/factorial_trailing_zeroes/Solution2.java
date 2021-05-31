0b21583cab3fa90c5a038d6c8567937c531776ca
unknown-peter <unknown-peter@protonmail.com>
2021-05-31 10:05:25 +0800

    Factorial Trailing Zeroes other solution

--- src/main/java/com/algorithm/problems/factorial_trailing_zeroes/Solution2.java
+++ src/main/java/com/algorithm/problems/factorial_trailing_zeroes/Solution2.java
@@ -5,8 +5,16 @@ package com.algorithm.problems.factorial_trailing_zeroes;
 
 class Solution2 implements Solution {
 
+    /**
+     * 找0到n中5的个数。不断除以5，为含有一个5、两个5...的数字个数
+     */
     @Override
     public int trailingZeroes(int n) {
-        return 0;
+        int res = 0;
+        while (n > 0) {
+            res += n / 5;
+            n /= 5;
+        }
+        return res;
     }
 }
