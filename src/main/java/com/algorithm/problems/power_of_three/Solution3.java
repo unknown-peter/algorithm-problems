f8f98535581f65a09f73bff08c1ac33a29e282ea
unknown-peter <unknown-peter@protonmail.com>
2021-08-07 23:53:14 +0800

    Power of Three

--- src/main/java/com/algorithm/problems/power_of_three/Solution3.java
+++ src/main/java/com/algorithm/problems/power_of_three/Solution3.java
@@ -3,13 +3,13 @@
  */
 package com.algorithm.problems.power_of_three;
 
-class Solution2 implements Solution {
+class Solution3 implements Solution {
 
     /**
      * solution from leetcode
      */
     @Override
     public boolean isPowerOfThree(int n) {
-        return Integer.toString(n, 3).matches("^10*$");
+        return (Math.log10(n) / Math.log10(3)) % 1 == 0;
     }
 }
--- src/main/java/com/algorithm/problems/power_of_three/Solution4.java
+++ src/main/java/com/algorithm/problems/power_of_three/Solution4.java
@@ -3,13 +3,13 @@
  */
 package com.algorithm.problems.power_of_three;
 
-class Solution2 implements Solution {
+class Solution4 implements Solution {
 
     /**
      * solution from leetcode
      */
     @Override
     public boolean isPowerOfThree(int n) {
-        return Integer.toString(n, 3).matches("^10*$");
+        return n > 0 && 1162261467 % n == 0;
     }
 }
