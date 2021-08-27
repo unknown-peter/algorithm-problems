f100fbf3b6d5d944c0c7d9738311fdb0038d1546
unknown-peter <unknown-peter@protonmail.com>
2021-08-27 23:42:29 +0800

    Binary Watch other solution

--- src/main/java/com/algorithm/problems/binary_watch/Solution2.java
+++ src/main/java/com/algorithm/problems/binary_watch/Solution2.java
@@ -3,12 +3,36 @@
  */
 package com.algorithm.problems.binary_watch;
 
+import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.List;
 
 class Solution2 implements Solution {
 
     @Override
     public List<String> readBinaryWatch(int turnedOn) {
-        return null;
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 60; j++) {
+                if (getBits(i, j) == turnedOn) {
+                    list.add(i + ":" + ((j < 10) ? ("0" + j) : j));
+                }
+            }
+        }
+        return list;
+    }
+
+    private int getBits(int hour, int minute) {
+        int time = (hour << 6) + minute;
+        BitSet bitSet = new BitSet(10);
+        int i = 0;
+        while (time != 0) {
+            if ((time & 0x00000001) == 0x00000001) {
+                bitSet.set(i);
+            }
+            time = time >> 1;
+            i++;
+        }
+        return bitSet.cardinality();
     }
 }
