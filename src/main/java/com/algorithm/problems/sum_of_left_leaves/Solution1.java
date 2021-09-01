297b6cc69f20eab59d6e611bd3677da222555b4d
unknown-peter <unknown-peter@protonmail.com>
2021-09-01 18:46:44 +0800

    Sum of Left Leaves other solution

--- src/main/java/com/algorithm/problems/sum_of_left_leaves/Solution1.java
+++ src/main/java/com/algorithm/problems/sum_of_left_leaves/Solution1.java
@@ -11,15 +11,9 @@ class Solution1 implements Solution {
     public int sumOfLeftLeaves(TreeNode root) {
         if (root == null) return 0;
 
-        int sum = 0;
-        if (root.left != null) {
-            if (root.left.left == null && root.left.right == null) sum += root.left.val;
-            else sum += sumOfLeftLeaves(root.left);
-        }
-        if (root.right != null) {
-            sum += sumOfLeftLeaves(root.right);
-        }
-        return sum;
+        if (root.left != null && root.left.left == null && root.left.right == null)
+            return root.left.val + sumOfLeftLeaves(root.right);
+        return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
     }
 
 }
--- src/main/java/com/algorithm/problems/sum_of_left_leaves/Solution2.java
+++ src/main/java/com/algorithm/problems/sum_of_left_leaves/Solution2.java
@@ -5,10 +5,26 @@ package com.algorithm.problems.sum_of_left_leaves;
 
 import com.ciaoshen.leetcode.util.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Solution2 implements Solution {
 
     @Override
     public int sumOfLeftLeaves(TreeNode root) {
-        return 0;
+        int sum = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node.left != null) {
+                if (node.left.left == null && node.left.right == null) sum += node.left.val;
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+        return sum;
     }
 }
