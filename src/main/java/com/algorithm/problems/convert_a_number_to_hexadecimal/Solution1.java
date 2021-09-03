4f4c7ee30087eef8625168220975fc3719ba79a3
unknown-peter <unknown-peter@protonmail.com>
2021-09-03 17:31:36 +0800

    Convert a Number to Hexadecimal modify

--- src/main/java/com/algorithm/problems/convert_a_number_to_hexadecimal/Solution1.java
+++ src/main/java/com/algorithm/problems/convert_a_number_to_hexadecimal/Solution1.java
@@ -5,14 +5,14 @@ package com.algorithm.problems.convert_a_number_to_hexadecimal;
 
 class Solution1 implements Solution {
 
-    private char[] hexArray = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+    private String hexString = "0123456789abcdef";
 
     @Override
     public String toHex(int num) {
         StringBuilder hex = new StringBuilder();
         if (num == 0) return "0";
         while (num != 0) {
-            hex.insert(0, hexArray[num & 0x0000000f]);
+            hex.insert(0, hexString.charAt(num & 0x0000000f));
             num >>>= 4;
         }
 
