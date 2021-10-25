4232014dcf7f9835eae5fb9eb2eab46f6f1db212
unknown-peter <unknown-peter@protonmail.com>
2021-10-25 09:35:17 +0800

    Maximum Depth of N-ary Tree other solution

--- src/main/java/com/algorithm/problems/maximum_depth_of_n_ary_tree/Solution2.java
+++ src/main/java/com/algorithm/problems/maximum_depth_of_n_ary_tree/Solution2.java
@@ -5,10 +5,24 @@ package com.algorithm.problems.maximum_depth_of_n_ary_tree;
 
 import com.algorithm.util.Node;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Solution2 implements Solution {
 
     @Override
     public int maxDepth(Node root) {
-        return 0;
+        if (root == null) return 0;
+        int depth = 0;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            depth++;
+            for (int i = queue.size(); i > 0; i--) {
+                Node node = queue.poll();
+                queue.addAll(node.children);
+            }
+        }
+        return depth;
     }
 }
