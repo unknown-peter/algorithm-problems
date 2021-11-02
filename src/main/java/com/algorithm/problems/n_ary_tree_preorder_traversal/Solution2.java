ec3cbb91d5b8351f8a7889dd672292583e4b8dcb
unknown-peter <unknown-peter@protonmail.com>
2021-11-02 21:29:43 +0800

    N-ary Tree Preorder Traversal other solution

--- src/main/java/com/algorithm/problems/n_ary_tree_preorder_traversal/Solution2.java
+++ src/main/java/com/algorithm/problems/n_ary_tree_preorder_traversal/Solution2.java
@@ -5,12 +5,26 @@ package com.algorithm.problems.n_ary_tree_preorder_traversal;
 
 import com.algorithm.util.Node;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 class Solution2 implements Solution {
 
     @Override
     public List<Integer> preorder(Node root) {
-        return null;
+        List<Integer> resList = new ArrayList<>();
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node node = stack.pop();
+            resList.add(node.val);
+            if (node.children != null) {
+                for (int i = node.children.size() - 1; i >= 0; i--) {
+                    if (node.children.get(i) != null) stack.push(node.children.get(i));
+                }
+            }
+        }
+        return resList;
     }
 }
