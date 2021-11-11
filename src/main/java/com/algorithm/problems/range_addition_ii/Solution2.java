7e0df4c1876025d512d00b3b13b2ecb9726eff8c
unknown-peter <unknown-peter@protonmail.com>
2021-11-11 08:11:27 +0800

    Range Addition II other solution

--- src/main/java/com/algorithm/problems/range_addition_ii/Solution2.java
+++ src/main/java/com/algorithm/problems/range_addition_ii/Solution2.java
@@ -7,6 +7,15 @@ class Solution2 implements Solution {
 
     @Override
     public int maxCount(int m, int n, int[][] ops) {
-        return 0;
+        if (ops.length == 0) return m * n;
+        if (ops.length == 1) return ops[0][0] * ops[0][1];
+
+        int r = m;
+        int c = n;
+        for (int i = 0; i < ops.length; i++) {
+            r = Math.min(r, ops[i][0]);
+            c = Math.min(c, ops[i][1]);
+        }
+        return r * c;
     }
 }
