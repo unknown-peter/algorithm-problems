ab6c990ba799e61d472d3cce2d03a97c5a4b7a18
unknown-peter <unknown-peter@protonmail.com>
2021-12-11 23:50:09 +0800

    Kth Largest Element in a Stream initial

---
+++ src/main/java/com/algorithm/problems/kth_largest_element_in_a_stream/Solution2.java
@@ -0,0 +1,21 @@
+/**
+ * Leetcode - kth_largest_element_in_a_stream
+ */
+package com.algorithm.problems.kth_largest_element_in_a_stream;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    
+
+}
