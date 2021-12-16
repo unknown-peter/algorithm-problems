56d392edaf36ab4c0dbec87d3d4dac7d7feb736d
unknown-peter <unknown-peter@protonmail.com>
2021-12-16 08:28:24 +0800

    Binary Tree Maximum Path Sum opt

--- src/main/java/com/algorithm/problems/binary_tree_maximum_path_sum/Solution1.java
+++ src/main/java/com/algorithm/problems/binary_tree_maximum_path_sum/Solution1.java
@@ -18,12 +18,9 @@ class Solution1 implements Solution {
 
     private int getMaxPathSum(TreeNode node) {
         if (node == null) return 0;
-        int left = getMaxPathSum(node.left);
-        int right = getMaxPathSum(node.right);
-        if (left < 0 && right < 0) maxSum = Math.max(maxSum, node.val);
-        else if (left < 0) maxSum = Math.max(maxSum, node.val + right);
-        else if (right < 0) maxSum = Math.max(maxSum, node.val + left);
-        else maxSum = Math.max(maxSum, left + node.val + right);
-        return (left < 0 && right < 0) ? node.val : node.val + Math.max(left, right);
+        int left = Math.max(getMaxPathSum(node.left), 0);
+        int right = Math.max(getMaxPathSum(node.right), 0);
+        maxSum = Math.max(maxSum, left + node.val + right);
+        return node.val + Math.max(left, right);
     }
 }
