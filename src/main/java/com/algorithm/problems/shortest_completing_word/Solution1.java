06f736cd56df2493f753f3bdbab02beaff7a4193
unknown-peter <unknown-peter@protonmail.com>
2021-12-31 11:11:59 +0800

    Shortest Completing Word opt

--- src/main/java/com/algorithm/problems/shortest_completing_word/Solution1.java
+++ src/main/java/com/algorithm/problems/shortest_completing_word/Solution1.java
@@ -8,29 +8,27 @@ class Solution1 implements Solution {
     @Override
     public String shortestCompletingWord(String licensePlate, String[] words) {
         int minLength = Integer.MAX_VALUE;
-        int[] letterCount = new int[26];
-        int[] wordLength = new int[words.length];
+        int[] licensePlateCount = new int[26];
+        String result = null;
+        char[] licensePlateArray = licensePlate.toLowerCase().toCharArray();
+        for (char letter : licensePlateArray) {
+            if (Character.isAlphabetic(letter)) licensePlateCount[letter - 'a']++;
+        }
+        for (int i = 0; i < words.length; i++) {
+            if (isCompletingWord(words[i], licensePlateCount) && words[i].length() < minLength) {
+                minLength = Math.min(minLength, words[i].length());
+                result = words[i];
+            }
+        }
+        return result;
+    }
 
-        for (char letter : licensePlate.toCharArray()) {
-            if (Character.isAlphabetic(letter)) letterCount[Character.toLowerCase(letter) - 'a']++;
+    private boolean isCompletingWord(String word, int[] licensePlateCount) {
+        int[] wordLetterCount = new int[26];
+        for (char letter : word.toCharArray()) wordLetterCount[letter - 'a']++;
+        for (int i = 0; i < 26; i++) {
+            if (wordLetterCount[i] < licensePlateCount[i]) return false;
         }
-        for (int i = 0; i < words.length; i++) {
-            int[] wordLetterCount = new int[26];
-            for (char letter : words[i].toCharArray()) {
-                wordLetterCount[Character.toLowerCase(letter) - 'a']++;
-            }
-            int j = 0;
-            for (; j < 26; j++) {
-                if (wordLetterCount[j] < letterCount[j]) break;
-            }
-            if (j == 26) {
-                wordLength[i] = words[i].length();
-                minLength = Math.min(minLength, wordLength[i]);
-            }
-        }
-        for (int i = 0; i < words.length; i++) {
-            if (wordLength[i] == minLength) return words[i];
-        }
-        return "";
+        return true;
     }
 }
