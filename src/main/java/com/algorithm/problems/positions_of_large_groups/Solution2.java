d4ef0bae74fd8e90917f59cf0e4100108db70985
unknown-peter <unknown-peter@protonmail.com>
2022-01-18 07:49:56 +0800

    Positions of Large Groups other solution

--- src/main/java/com/algorithm/problems/positions_of_large_groups/Solution2.java
+++ src/main/java/com/algorithm/problems/positions_of_large_groups/Solution2.java
@@ -3,12 +3,28 @@
  */
 package com.algorithm.problems.positions_of_large_groups;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class Solution2 implements Solution {
 
+    /**
+     * solution from leetcode
+     */
     @Override
     public List<List<Integer>> largeGroupPositions(String s) {
-        return null;
+        List<List<Integer>> ans = new ArrayList();
+        int i = 0, N = s.length(); // i is the start of each group
+        for (int j = 0; j < N; ++j) {
+            if (j == N - 1 || s.charAt(j) != s.charAt(j + 1)) {
+                // Here, [i, j] represents a group.
+                if (j - i + 1 >= 3)
+                    ans.add(Arrays.asList(new Integer[]{i, j}));
+                i = j + 1;
+            }
+        }
+
+        return ans;
     }
 }
