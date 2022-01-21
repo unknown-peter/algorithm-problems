d68f191eaf044ea50498fe45278ee728f54e55d9
unknown-peter <unknown-peter@protonmail.com>
2022-01-21 08:38:38 +0800

    Backspace String Compare other solution

--- src/main/java/com/algorithm/problems/backspace_string_compare/Solution2.java
+++ src/main/java/com/algorithm/problems/backspace_string_compare/Solution2.java
@@ -3,10 +3,21 @@
  */
 package com.algorithm.problems.backspace_string_compare;
 
+import java.util.Stack;
+
 class Solution2 implements Solution {
 
     @Override
     public boolean backspaceCompare(String s, String t) {
-        return false;
+        return parseStr(s).equals(parseStr(t));
+    }
+
+    private String parseStr(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : str.toCharArray()) {
+            if (ch != '#') stack.push(ch);
+            else if (!stack.isEmpty()) stack.pop();
+        }
+        return String.valueOf(stack);
     }
 }
