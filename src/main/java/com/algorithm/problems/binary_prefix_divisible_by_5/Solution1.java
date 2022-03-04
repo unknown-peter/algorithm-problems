533a64b6052baafe3a4bcd90c4dd26ab98ef9060
unknown-peter <unknown-peter@protonmail.com>
2022-03-04 10:18:20 +0800

    Binary Prefix Divisible By 5 solution 1

--- src/main/java/com/algorithm/problems/binary_prefix_divisible_by_5/Solution1.java
+++ src/main/java/com/algorithm/problems/binary_prefix_divisible_by_5/Solution1.java
@@ -8,14 +8,17 @@ import java.util.List;
 
 class Solution1 implements Solution {
 
+    /**
+     * 当前数为x，则下一个数为2x或2x+1
+     * 根据求余性质，保存每次计算之后对5取余的结果
+     */
     @Override
     public List<Boolean> prefixesDivBy5(int[] nums) {
         List<Boolean> list = new ArrayList<>();
-        long curNum = 0;
+        int curNum = 0;
         for (int num : nums) {
-            curNum = (curNum << 1) + num;
-            if (curNum % 5 == 0) list.add(true);
-            else list.add(false);
+            curNum = ((curNum << 1) + num) % 5;
+            list.add(curNum == 0);
         }
         return list;
     }
