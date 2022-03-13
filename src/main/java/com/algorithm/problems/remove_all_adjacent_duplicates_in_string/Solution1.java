90c92a8f0860d79e0f545dea8e2b1a1590378827
unknown-peter <unknown-peter@protonmail.com>
2022-03-13 23:36:06 +0800

    Remove All Adjacent Duplicates In String solution 1

--- src/main/java/com/algorithm/problems/remove_all_adjacent_duplicates_in_string/Solution1.java
+++ src/main/java/com/algorithm/problems/remove_all_adjacent_duplicates_in_string/Solution1.java
@@ -7,6 +7,14 @@ class Solution1 implements Solution {
 
     @Override
     public String removeDuplicates(String s) {
+        if (s.length() == 1) return s;
 
+        char[] res = new char[s.length()];
+        int i = 0;
+        for (char ch : s.toCharArray()) {
+            if (i != 0 && res[i - 1] == ch) i -= 1;
+            else res[i++] = ch;
+        }
+        return new String(res, 0, i);
     }
 }
