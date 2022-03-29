0c6ce89b8eff586e3bd3a57578cb9fc7458dfee6
unknown-peter <unknown-peter@protonmail.com>
2022-03-29 09:27:46 +0800

    Add Two Numbers optimize solution

--- src/main/java/com/algorithm/problems/add_two_numbers/Solution1.java
+++ src/main/java/com/algorithm/problems/add_two_numbers/Solution1.java
@@ -12,23 +12,16 @@ class Solution1 implements Solution {
         int carry = 0;
         ListNode head = new ListNode(-1);
         ListNode node = head;
-        int tmp;
         while (l1 != null || l2 != null) {
-            if (l1 == null) {
-                tmp = l2.val + carry;
-                l2 = l2.next;
-            } else if (l2 == null) {
-                tmp = l1.val + carry;
-                l1 = l1.next;
-            } else {
-                tmp = l1.val + l2.val + carry;
-                l1 = l1.next;
-                l2 = l2.next;
-            }
+            int v1 = (l1 != null) ? l1.val : 0;
+            int v2 = (l2 != null) ? l2.val : 0;
+            int sum = carry + v1 + v2;
+            carry = sum / 10;
 
-            node.next = new ListNode(tmp % 10);
+            node.next = new ListNode(sum % 10);
             node = node.next;
-            carry = tmp / 10;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
         }
         if (carry > 0) node.next = new ListNode(carry);
         return head.next;
