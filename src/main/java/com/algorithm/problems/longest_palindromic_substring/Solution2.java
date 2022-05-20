7825e396c648d03f71c5d9ccc72744472319eeb1
unknown-peter <unknown-peter@protonmail.com>
2022-05-20 22:24:49 +0800

    Longest Palindromic Substring other solution

--- src/main/java/com/algorithm/problems/longest_palindromic_substring/Solution2.java
+++ src/main/java/com/algorithm/problems/longest_palindromic_substring/Solution2.java
@@ -5,8 +5,26 @@ package com.algorithm.problems.longest_palindromic_substring;
 
 class Solution2 implements Solution {
 
+    /**
+     * 对于字符串构建dp数组，dp[i][j]表示字符串s从i到j子串是否为回文串
+     * 对于dp[i][j]，如果dp[i+1][j-1]是回文串而且s[i]==s[j]，则dp[i][j]为回文串
+     */
     @Override
     public String longestPalindrome(String s) {
-        return null;
+        if (s == null || (s.length() == 0)) return "";
+
+        int l = s.length();
+        boolean[][] dp = new boolean[l][l];
+        String res = "";
+        for (int j = 0; j < l; j++) {
+            for (int i = 0; i <= j; i++) {
+                if (i == j) dp[i][j] = true;
+                else if (((i + 1) == j) && s.charAt(i) == s.charAt(j)) dp[i][j] = true;
+                else if (dp[i + 1][j - 1] && s.charAt(i) == s.charAt(j)) dp[i][j] = true;
+
+                if (dp[i][j] && res.length() < (j - i + 1)) res = s.substring(i, j + 1);
+            }
+        }
+        return res;
     }
 }
---
+++ src/main/java/com/algorithm/problems/longest_palindromic_substring/Solution3.java
@@ -0,0 +1,36 @@
+/**
+ * Leetcode - longest_palindromic_substring
+ */
+package com.algorithm.problems.longest_palindromic_substring;
+
+class Solution3 implements Solution {
+
+    /**
+     * solution from leetcode
+     * 回文串关于中心对称，因此可以从中心开始拓展，直到不满足回文串条件或到达边界
+     */
+    @Override
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+}
