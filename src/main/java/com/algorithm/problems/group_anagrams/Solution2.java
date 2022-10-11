8c847a4838f78cb1590c4d5bd4c04b0ac8cea578
unknown-peter <unknown-peter@protonmail.com>
2022-10-11 12:15:02 +0800

    Group Anagrams other solution

--- src/main/java/com/algorithm/problems/group_anagrams/Solution2.java
+++ src/main/java/com/algorithm/problems/group_anagrams/Solution2.java
@@ -38,4 +38,16 @@ class Solution2 implements Solution {
         }
         return new ArrayList(ans.values());
     }
+
+    //    public List<List<String>> groupAnagrams(String[] strs) {
+    //        Map<String, List> ans = new HashMap<>();
+    //        for (String s : strs) {
+    //            char[] count = new char[26];
+    //            for (char c : s.toCharArray()) count[c - 'a']++;
+    //            String key = new String(count);
+    //            if (!ans.containsKey(key)) ans.put(key, new ArrayList());
+    //            ans.get(key).add(s);
+    //        }
+    //        return new ArrayList(ans.values());
+    //    }
 }
